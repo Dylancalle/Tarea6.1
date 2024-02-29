ee602a9de54008bcd56acf6bd0fbc58871b028ff
Dylan Calle Contreras <dylan.calle@ucb.edu.bo>
2024-02-29 06:58:14 -0400

    los divisibles por 100, pero no por 400, no son bisiestos

--- src/esBisiesto.js
+++ src/esBisiesto.js
@@ -1,9 +1,13 @@
 function esBisiesto(n) {
-  if(n%4==0)
-  {
-    return true;
-  }
-  return false;
+    if(n%100==0 && n%400!==0)
+    {
+     return false;
+    }
+    if(n%4 == 0)
+    {
+     return true;
+    }
+    return false;
 }
 
   export default esBisiesto;
--- src/esBisiesto.spec.js
+++ src/esBisiesto.spec.js
@@ -7,4 +7,7 @@ describe("Es bisiesto", () => {
     it("deberia considerar al 4 y a los multiplos de 4", () => {
         expect(esBisiesto(8)).toEqual(true);
       });
+    it("deberia considerar a los años divisibles por 100, pero no por 400", () => {
+    expect(esBisiesto(1700)).toEqual(false);
+});
   });
